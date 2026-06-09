f43aac778bf234a1dc5f04b82e47c76136441b13
Deepak kumar <kumardeepak46263@gmail.com>
2026-06-09 12:50:56 +0530

    some little changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -74,6 +74,11 @@ export const singUp = async (req, res) => {
 
 export const Login = async (req, res) => {
     const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" })
+    }
+
     try {
         const user = await User.findOne({ email: email })
         if (!user) {
