a8234579c7aea8702b576b241a5138871f84aca5
Deepak kumar <kumardeepak46263@gmail.com>
2026-06-10 15:30:38 +0530

    Feat: Sucessfully Added Message.model.js file

---
+++ backend/src/models/Message.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    recieverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text: {   // this is for message which send by sender to reciever
+        type: String,
+        maxLength: 500
+    },
+    image: {
+        type: String
+    }
+}, { timestamps: true })
+
+
+const Message = mongoose.model("Message", messageSchema)
+export default Message
